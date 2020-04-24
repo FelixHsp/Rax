48a5900a832bf3612f7c3d82a67f9d9c41f20616
Felix <13115412392@163.com>
2020-04-24 16:16:13 +0800

    feat:引入threejs

--- my-project/src/pages/Home/index.tsx
+++ my-project/src/pages/Home/index.tsx
@@ -5,14 +5,14 @@ import Text from 'rax-text';
 import './index.css';
 
 import Logo from '../../components/Logo';
+import Three from '../../components/Three';
 
 export default function Home() {
-  return (
-    <View className="home">
-      <Logo uri="//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png" />
-      <Text className="title">Welcome to Your Rax App</Text>
-      <Text className="info">More information about Rax</Text>
-      <Text className="info">Visit https://rax.js.org</Text>
-    </View>
-  );
+    return (
+        <View className="home">
+            <Logo uri="//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png" />
+            <Text className="title">Three.js Demo</Text>
+            <Three />
+        </View>
+    );
 }
